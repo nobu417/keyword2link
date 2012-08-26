ee5bd4210015dcc361666141a39eef925df891f9
Nobuyuki Sato <nobu417@me.com>
2012-08-27 05:01:53 +0900

    Amazon検索とGoogle検索を追加。 e.x. [a[keyword]] -> make a link of keyword to amazon.co.jp [g[keyword]] -> make a link of keyword to search Google.

--- keyword2link.pl
+++ keyword2link.pl
@@ -12,9 +12,22 @@ MT::Template::Context->add_global_filter(keyword2link => \&keyword2link);
 
 sub keyword2link {
 	my ($text, $arg, $ctx) = @_;
-	# Rewrite URL below to your search URL.
+
+	# Set your Amazon associate below.
+	my $amazon_tag = "nobu417jp-22";
+	# Set the search URL of your site below.
 	my $url = "http://www.nobu417.jp/search?q=";
-	$text =~ s/\[\[([^\]]*)\]\]/<a href="$url$1" class="keyword">$1<\/a>/ig;
+
+	my $amazon = "http://www.amazon.co.jp/gp/search?index=blended&tag=$amazon_tag&keywords=";	
+	my $google = "http://www.google.com/search?q=";
+
+	if ($text =~ /\[a\[([^\]]*)\]\]/) {
+		$text =~ s/\[a\[([^\]]*)\]\]/<a href="$amazon$1" class="amazon">$1<\/a>/ig;
+	} elsif ($text =~ /\[g\[([^\]]*)\]\]/) {
+		$text =~ s/\[g\[([^\]]*)\]\]/<a href="$google$1" class="google">$1<\/a>/ig;
+	} else {
+		$text =~ s/\[\[([^\]]*)\]\]/<a href="$url$1" class="keyword">$1<\/a>/ig;
+	}
 	$text;
 }
 
